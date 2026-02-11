01391da90bde11518c131103164d45fe60a3bc20
Neha K <63655853+neha-github-art@users.noreply.github.com>
2026-02-11 22:29:02 +0530

    Add instructions to clone and run the project

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# Clone the repository
+git clone https://github.com/neha-github-art/my-python-project.git
 
+# Navigate to the folder
+cd my-python-project
+
+# View the file
+cat main.py
+
+# Run the file
+python main.py
